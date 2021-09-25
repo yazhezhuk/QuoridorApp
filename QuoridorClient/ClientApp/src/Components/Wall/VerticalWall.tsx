a9e0b2fe67a8ce970ee8c765d3c8ed28a48de588
Aziull <65764457+Aziull@users.noreply.github.com>
2021-09-25 17:46:38 +0300

    Wall customizing

--- QuoridorClient/ClientApp/src/Components/Wall/VerticalWall.tsx
+++ QuoridorClient/ClientApp/src/Components/Wall/VerticalWall.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import {  CustomPIXIComponent } from "react-pixi-fiber";
+import {Graphics} from "pixi.js";
 
-const VerticalWall = () => {
-    return(
-        <div>
-            Vertical Wall
-        </div>
-    )
-}
+interface Props {
+    position: { x: number; y: number };
+    color: number;
+    width: number;
+    height: number;
+  }
+
+export default CustomPIXIComponent<Graphics,Props>({
+
+  customDisplayObject: () => new Graphics(),
+  customApplyProps: function(instance, oldProps, newProps) {
+   
+    const { color, position, width, height, ...newPropsRest } = newProps;
+    const { color: oldFColor, position: oldPosition, width: oldWidth, height: oldHeight, ...oldPropsRest } = oldProps;
+    const {x,y} = position
+    if (typeof oldProps !== "undefined") {
+      instance.clear();
+    }
+    instance.beginFill(0xeee);
+    instance.drawRect(x, y, width, height);
+    instance.endFill();
+  },
+}, 'VerticalWall')
