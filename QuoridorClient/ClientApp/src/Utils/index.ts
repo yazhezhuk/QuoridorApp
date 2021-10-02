190f2d71020f63ebc4fede55d27f80602a5362f1
YevgeniyBukurIT <85176058+YevgeniyBukurIT@users.noreply.github.com>
2021-10-02 13:59:57 +0300

    Utils update v.1

--- QuoridorClient/ClientApp/src/Utils/index.ts
+++ QuoridorClient/ClientApp/src/Utils/index.ts
@@ -1,6 +1,33 @@
 //тут будуть типи, які потрібно
+
+export enum Color {
+  lightGray = 0xfffafa,
+  lavender = 0xE6E6FA,
+  white = 0xFFFFFF,
+  black = 0x000000
+
+}
+
+export const Theme ={
+  wallColor: Color.lightGray,
+  cellColor: Color.lavender 
+}
 export type Player = {
   id: number;
   numOfWallLeft: number;
   stepSize: number;
+};
+export type Step = {
+  walls: { x: number; y: number }[];
+  player0: {
+    x: number;
+    y: number;
+    remainingWalls: number;
+  };
+  player1: {
+    x: number;
+    y: number;
+    remainingWalls: number;
+  };
+  stepNumber: number;
 };
