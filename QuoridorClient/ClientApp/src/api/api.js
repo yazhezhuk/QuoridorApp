5d27b7df32b637a68eedcf26064465586bd0bfdc
YevgeniyBukurIT <85176058+YevgeniyBukurIT@users.noreply.github.com>
2021-10-13 23:25:02 +0300

    playerAPI

--- QuoridorClient/ClientApp/src/api/api.js
+++ QuoridorClient/ClientApp/src/api/api.js
@@ -3,6 +3,17 @@ export const moveAPI = {
         
     }
 }
+const URL = "https://localhost:5000/";
+
+export const playerAPI = {
+    async tryMove(cellFrom , cellTo, turn, opponent, walls) {
+        const response = await fetch(URL + "move/" ,
+            {method: "GET",
+            body: JSON.stringify(cellFrom, cellTo, turn, opponent, walls)})
+        return await response.json()
+    },
+}
+
 
 
 
