fe38ce0c199773781b5bc560bfb04ae040666516
YevgeniyBukurIT <85176058+YevgeniyBukurIT@users.noreply.github.com>
2021-10-17 22:02:07 +0300

    Update api.js

--- QuoridorClient/ClientApp/src/api/api.js
+++ QuoridorClient/ClientApp/src/api/api.js
@@ -1,19 +1,65 @@
+import {WallToSend} from "../Utils";
+
 export const moveAPI = {
     makeMove() {
-        
+
     }
 }
-const URL = "https://localhost:5000/";
+const URL = "http://localhost:9000/";
 
-export const playerAPI = {
-    async tryMove(cellFrom , cellTo, turn, opponent, walls) {
-        const response = await fetch(URL + "move/" ,
-            {method: "GET",
-            body: JSON.stringify(cellFrom, cellTo, turn, opponent, walls)})
-        return await response.json()
-    },
+export interface Position {
+    X : number,
+    Y : number
+}
+export interface WallPosition {
+    X : number,
+    Y : number,
+    Direction : string
 }
 
-
-
-
+export const playerAPI = {
+    async tryMove(cellFrom: Position,
+                  cellTo: Position,
+                  turn: number,
+                  opponent: Position,
+                  walls: WallToSend[]) {
+        const response = await fetch(URL + "move",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'text/json',
+                },
+                body: JSON.stringify({
+                    cellFrom,
+                    cellTo,
+                    turn,
+                    opponent,
+                    walls
+                }),
+            }
+        )
+        return (await response.json())["CanExecute"];
+    },
+    async tryWall(current: Position,
+                  opponent: Position,
+                  turn: number,
+                  toSetup: WallToSend,
+                  otherWalls: WallToSend[]) {
+        const response = await fetch(URL + "move/wall/",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'text/json',
+                },
+                body: JSON.stringify({
+                    current,
+                    opponent,
+                    turn,
+                    toSetup,
+                    otherWalls
+                }),
+            }
+        )
+        return (await response.json())["CanExecute"];
+    },
+}
